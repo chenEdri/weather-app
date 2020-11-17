322c2aaaf7be1d7b73725cbb907e72965309eb11
chen edri <chenedri22@gmail.com>
2020-11-17 13:16:49 +0200

    Updates

---
+++ precache-manifest.529e9f3c53803a9ca5614f3cd52a193b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a77c9f6b433b848a78f6be598ceef0",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "74b8e793645eb6508c6e",
+    "url": "/weather-app/static/css/main.7fe0d3f5.chunk.css"
+  },
+  {
+    "revision": "49bbfcfd8658fff2a5dc",
+    "url": "/weather-app/static/js/2.51e53abe.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/weather-app/static/js/2.51e53abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b8e793645eb6508c6e",
+    "url": "/weather-app/static/js/main.f6f3c75d.chunk.js"
+  },
+  {
+    "revision": "6217e7c726a47d063da4",
+    "url": "/weather-app/static/js/runtime-main.8ce43dfd.js"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/weather-app/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "fddc5fa6efb91e49befb6ad849da9b0f",
+    "url": "/weather-app/static/media/Livvic-Bold.fddc5fa6.ttf"
+  },
+  {
+    "revision": "fe84dbc6ce317e6216d959cf7baed69c",
+    "url": "/weather-app/static/media/Livvic-SemiBold.fe84dbc6.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/weather-app/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "21a4149586d82c230856dab913d2770c",
+    "url": "/weather-app/static/media/logo.21a41495.png"
+  }
+]);
